6cd3f8d40c0a403b730fcb8600987c7c66858725
Nitin <nitinkmr.4169@gmail.com>
2025-06-12 19:13:53 +0530

    Made a component UserCard

---
+++ react-project-two/src/Components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+      <p id='user-title'>Nitin bhaiya</p>
+      <img id='user-img' src="" alt="Rao" />
+      <p id='user-desc'>Description of Nitin bhaiya</p>
+    </div>
+  )
+}
+
+export default UserCard 
